b5c8c727c8984e3ff9f48ab8737435426bb47c7a
Kristiyana Nikolova <57190703+Kristiyana00@users.noreply.github.com>
2020-11-22 10:17:09 +0200

    Add files via upload

---
+++ Week4/4.js
@@ -0,0 +1,10 @@
+class Person{
+        getSecretSalaryInfo(){
+        let _salary = 1000;
+        function printSalary()
+        {
+            return _salary;
+        }
+        console.log(printSalary());
+    }
+}
